fb0bf1d81d0bdfa435841134cdfb969a358eed2b
Marcos Santiago Rivera (CTS-SI) <msantiago@ocpr.gov.pr>
2023-05-16 09:32:55 -0400

    envs

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ const User = require("./models/user");
 const savedPublications = require("./routes/savedPublications/savedPublications");
 //"mongodb://localhost:27017/PFdummy"
 //process.env.DBURL
-const dbUrl = "mongodb://127.0.0.1:27017/PFdummy" || process.env.DBURL;
+const dbUrl = process.env.DBURL || "mongodb://127.0.0.1:27017/PFdummy";
 mongoose.connect(dbUrl, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
